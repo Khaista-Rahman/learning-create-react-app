07571500caafa91207363b10cf389dbfaad29065
Khaista-Rahman <Khaistarahman.cs@gmail.com>
2024-04-22 08:50:19 +0000

    little work by myself

---
+++ src/khaista.js
@@ -0,0 +1,15 @@
+import Chai from './App.js'
+
+
+function Khaista(){
+  return(
+    <>
+
+    <h1 className='bg-cyan-600 rounded-xl p-7 w-fit'>
+      hey buddy! how are you i am khaista rahman and where you from?
+    </h1>
+    </>
+  )
+}
+
+export default Khaista;
---
+++ src/shahid.js
@@ -0,0 +1,9 @@
+function Shahid(){
+  return(
+    <h1 className='bg-emerald-500 text-white font-extrabold p-5 rounded-xl w-fit'>
+      hey buddy! i am shahid zada .
+    </h1>
+  )
+}
+
+export default Shahid;
